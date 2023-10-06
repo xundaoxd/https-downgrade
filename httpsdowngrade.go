f77ce55599a41071b60a8b848d1adc45d5203742
xundaoxd <xundaoxd@outlook.com>
2023-10-07 00:46:36 +0800

    tab to sapce

--- httpsdowngrade.go
+++ httpsdowngrade.go
@@ -14,28 +14,28 @@ type Config struct {
 
 // CreateConfig creates the default plugin configuration.
 func CreateConfig() *Config {
-	return &Config{
+    return &Config{
         Headers: make(map[string]string),
     }
 }
 
 // HTTPSDowngrade a HTTPSDowngrade plugin.
 type HTTPSDowngrade struct {
-	next     http.Handler
+    next     http.Handler
     headers  map[string]string
-	name     string
+    name     string
 }
 
 // New created a new HTTPSDowngrade plugin.
 func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
-	return &HTTPSDowngrade{
+    return &HTTPSDowngrade{
         headers:  config.Headers,
-		next:     next,
-		name:     name,
-	}, nil
+        next:     next,
+        name:     name,
+    }, nil
 }
 
 func (a *HTTPSDowngrade) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
     os.Stdout.WriteString("HTTPSDowngrade")
-	a.next.ServeHTTP(rw, req)
+    a.next.ServeHTTP(rw, req)
 }

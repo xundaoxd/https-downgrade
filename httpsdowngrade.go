2223c8559569d7c460fb6cdc2175c2311513160c
xundaoxd <xundaoxd@outlook.com>
2023-10-07 00:26:37 +0800

    add comment

--- httpsdowngrade.go
+++ httpsdowngrade.go
@@ -6,18 +6,22 @@ import (
 	"net/http"
 )
 
+// Config the plugin configuration.
 type Config struct {
 }
 
+// CreateConfig creates the default plugin configuration.
 func CreateConfig() *Config {
 	return &Config{}
 }
 
+// plugin.
 type HttpDowngrade struct {
 	next     http.Handler
 	name     string
 }
 
+// New created a new HttpDowngrade plugin.
 func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
 	return &HttpDowngrade{
 		next:     next,
